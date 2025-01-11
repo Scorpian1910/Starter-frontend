49aace1654896081e2f48fc8879e5a98d7ab4014
bhcoder3 <blackholeinfiverse3@gmail.com>
2025-01-11 10:55:23 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-blue-500 text-white text-center h-screen flex items-center justify-center">
+      <p className="text-3xl font-bold">TailwindCSS is Working!</p>
+    </div>
+  );
+};
+
+export default App;
